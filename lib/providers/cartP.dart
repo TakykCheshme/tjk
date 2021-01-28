d0cc122f978eaba9a4862988741d3a5b6c23d4d5
Kerim Amanov <amanokerim@Kerims-MacBook-Pro.local>
2021-01-28 19:08:27 +0500

    cart icon red dot and total price

--- lib/providers/cartP.dart
+++ lib/providers/cartP.dart
@@ -6,36 +6,47 @@ class CartP extends ChangeNotifier {
   List<CartItem> _items = [];
   List<CartItem> get items => _items;
 
-  int _totalPrice;
-  int get totalPrice => _totalPrice;
+  double _totalPrice;
+  double get totalPrice => _totalPrice;
+
+  void _calculateTotalPrice() {
+    _totalPrice = 0;
+    for (CartItem item in _items)
+      _totalPrice += item.product.price * item.count;
+  }
 
   void add(Product product, int count) {
     CartItem item = CartItem(product, count);
     if (!_items.contains(item))
       _items.add(item);
-    else {
+    else
       _items.firstWhere((i) => i == item).count++;
-    }
+
+    _calculateTotalPrice();
     notifyListeners();
   }
 
   void removeAt(int index) {
     _items.removeAt(index);
+    _calculateTotalPrice();
     notifyListeners();
   }
 
   void clear() {
     _items.clear();
+    _calculateTotalPrice();
     notifyListeners();
   }
 
   void increaseAt(int index) {
     _items[index].count = _items[index].count + 1;
+    _calculateTotalPrice();
     notifyListeners();
   }
 
   void decreaseAt(int index) {
     _items[index].count = _items[index].count - 1;
+    _calculateTotalPrice();
     notifyListeners();
   }
 }
--- lib/shared/tjk_nvigation_bar.dart
+++ lib/shared/tjk_nvigation_bar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tjk/providers/cartP.dart';
 import 'package:tjk/views/cartV.dart';
 
 import '../const.dart';
@@ -19,10 +21,32 @@ class TJKNavigationBar extends StatelessWidget
         overflow: TextOverflow.ellipsis,
       ),
       trailing: GestureDetector(
-          onTap: () => Navigator.of(context, rootNavigator: true).push(
-                CupertinoPageRoute(builder: (context) => CartV()),
-              ),
-          child: Icon(CupertinoIcons.cart, size: 28.0)),
+        onTap: () => Navigator.of(context, rootNavigator: true).push(
+          CupertinoPageRoute(builder: (context) => CartV()),
+        ),
+        child: Container(
+          width: 30.0,
+          child: Consumer<CartP>(
+            builder: (_, cart, __) => Stack(
+              children: [
+                Icon(CupertinoIcons.cart, size: 28.0),
+                Positioned(
+                  right: 0.0,
+                  top: 0.0,
+                  child: Container(
+                    padding: EdgeInsets.all(5.0),
+                    decoration: BoxDecoration(
+                      color:
+                          cart.items.isEmpty ? Colors.transparent : Colors.red,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 
